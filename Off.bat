e098801b14fb0b5fb633230faa48ba410146c86a
Super Eagle <47841908+superEH@users.noreply.github.com>
2020-07-25 16:46:00 -0500

    Add files via upload

---
+++ Off.bat
@@ -0,0 +1,34 @@
+@rem bin\windows\bat
+@echo off
+Color 6
+echo.
+echo Hola %USERNAME% 
+set Nombre=         Autor: SEH
+echo.                                                          
+echo                 iP@@@@@@@2     iOMBMBMBB@  5BMBMBB@B    
+echo               5@B@B@@@B@B@BU  i@B@B@B@B@B B@B@B@B@@@   
+echo             :B@B@@@SUuBB@B@Bv MB@B@M@@@Bi @B@@@B@B@,    
+echo            .B@B@M      .B@B@B B@B@       kB@Bv           
+echo            @@B@Z        7@@B@ MB@@,::ir: B@B@L,:ii:      
+echo           r@B@B         r@B@@ B@B@B@@@Bq @B@B@B@B@1         
+echo           E@@B@         @B@@2 @B@B@B@B@ LB@B@B@B@B       
+echo           L@B@B2      .@@@B@ O@@B       B@B@:             
+echo            B@B@B@7,,vM@B@B@ vB@BS      .@B@@           
+echo             B@B@B@B@B@B@BO  @@B@,      EB@BM            
+echo              r@B@B@B@B@F.  r@B@B       B@B@r             
+echo                 ,;;:.      ..          ,  .         
+echo 	    
+echo       %Nombre% 
+echo              DIA %date%  HORA %time%
+net user                                                    
+set /p tiempo=El Tiempo de apagar :
+shutdown -s -t %tiempo% 
+cls
+exit
+
+
+
+
+
+
+
